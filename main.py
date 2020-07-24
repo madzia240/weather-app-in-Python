d956d5ce642da2dd3810446b19f9555549806048
Magda Florek <madzia88f@o2.pl>
2020-07-24 18:25:27 +0200

    ui added

--- main.py
+++ main.py
@@ -2,18 +2,21 @@ from requests import get
 from json import loads
 from terminaltables import AsciiTable
 
+url = 'https://danepubliczne.imgw.pl/api/data/synop'
+response = get(url)
 
-cities = ['Wrocław']
+
+def cities():
+    cities = [row['stacja'] for row in loads(response.text)]
+    return cities
 
 
-def main():
-    url = 'https://danepubliczne.imgw.pl/api/data/synop'
-    response = get(url)
+def main(city):
     rows = [
         ['Miasto', 'Data', 'Godzina', 'Temperatura', 'Opady', 'Cisnienie']
     ]
     for row in loads(response.text):
-        if row['stacja'] in cities:
+        if row['stacja'] == city:
             rows.append([
                 row['stacja'], row['data_pomiaru'], row['godzina_pomiaru'], row['temperatura'], row['suma_opadu'], row['cisnienie']
             ])
@@ -23,4 +26,9 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    quit = 'Y'
+    cities = {index: city for index, city in enumerate(cities())}
+    while quit.upper() == 'Y':
+        city = int(input(f'Choose city: {cities}'))
+        main(cities[city])
+        quit = input('Again? [Y]es [N]o\n')
